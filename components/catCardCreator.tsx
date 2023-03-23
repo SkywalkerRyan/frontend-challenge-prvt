dcd3f324128be19cfa40814797c9d16c1dcdc0b1
Ryan Mansur <skywalker.ryan@gmail.com>
2023-03-23 06:02:21 +0600

    Random cat image.

--- components/catCardCreator.tsx
+++ components/catCardCreator.tsx
@@ -1,4 +1,43 @@
-const CatCardCreator = () => {
-  return <img src="catCreatorConcept.png" alt="CatCardCreator concept" />;
+import React, { useState, FC, ReactElement } from "react";
+
+import styles from "@/styles/CatCardCreator.module.css";
+
+const CAT_API_KEY = "live_tYTRfU3cmdp9RJX78Xx1JNDtt5jpOSMlz18ACdNkpw25R1j3vzzC1jsnWsKSv02e";
+const RANDOM_CAT_URL = "https://api.thecatapi.com/v1/images/search";
+
+const CatCardCreator: FC = (): ReactElement => {
+	const [catName, setCatName] = useState("");
+	const [catImg, setCatImg] = useState("");
+
+	const loadRandomCat = async () => {
+		try {
+			fetch(RANDOM_CAT_URL, {
+				headers: {
+					"x-api-key": CAT_API_KEY
+				}
+			})
+				.then(data => {
+					return data.json();
+				})
+				.then(data => {
+					setCatImg(data[0].url);
+				});
+		} catch (err) {
+			console.error("catCardCreator : loadRandomCat : err : ", err);
+		}
+	};
+
+	const saveCatToHerd = () => {
+		//
+	};
+
+	return (
+		<div className={styles.creator}>
+			<input className={styles.catNameBox} type="text" value={catName} placeholder="Cat Name" onChange={e => setCatName(e.target.value)} />
+			<img src={catImg} alt={catName} className={styles.catImageBox} />
+			<button onClick={loadRandomCat}>Refresh</button>
+			<button onClick={saveCatToHerd}>Save</button>
+		</div>
+	);
 };
 export default CatCardCreator;
